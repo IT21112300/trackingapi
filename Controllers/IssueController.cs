b34c7f2bb190a5462a107bfa4b7241c9154251af
IT21112300 <it21112300@my.sliit.lk>
2023-07-05 20:26:42 +0530

    Add project files.

---
+++ Controllers/IssueController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using trackingapi.Data;
+
+namespace trackingapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+
+        public IssueController(IssueDbContext context) => _context = context;
+
+    }
+}
